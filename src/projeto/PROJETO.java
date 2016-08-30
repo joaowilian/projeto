925fbda93d1b95381274aa03ac6313c10a803c3a
joao <joao@faculdade>
2016-08-29 21:10:55 -0300

    mensagem

--- src/projeto/PROJETO.java
+++ src/projeto/PROJETO.java
@@ -1,21 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package projeto;
 
-/**
- *
- * @author joao
- */
+import javax.swing.JOptionPane;
+
 public class PROJETO {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        JOptionPane.showMessageDialog(null,"muito loco");
+                
+        
+        
     }
     
 }
